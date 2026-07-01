0d6d73218dd0095404dcb2b099da0afc8fdcd259
DURGESH KUMAR DEWANGAN <github.durgesh@gmail.com>
2026-07-01 22:08:53 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()   # ← This line must exist at the top level (not inside a function)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
